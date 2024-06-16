d22156f54b481685cb7b36ed5ac68f9e752a213b
aldm <d44.a1@yandex.ru>
2024-06-17 01:10:02 +0300

    1

--- js/index.js
+++ js/index.js
@@ -6,3 +6,17 @@ burgerClick.addEventListener("click", () => {
   burgerClick.classList.toggle("active");
   scrollLock.classList.toggle("lock");
 });
+
+/***открытие правил***/
+const rentButton = document.querySelector(".rent__button");
+const overlay = document.getElementById("overlay");
+
+rentButton.addEventListener("click", () => {
+  overlay.style.display = "flex";
+});
+
+overlay.addEventListener("click", (event) => {
+  if (event.target === overlay) {
+    overlay.style.display = "none";
+  }
+});
